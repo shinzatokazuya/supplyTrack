80a3caa08efde1d840bbd9d2a1744a03dcddb245
Kazuya <shinzatokazuya11@gmail.com>
2025-06-02 21:23:02 +0000

    Mon, Jun 2, 2025, 6:23 PM -03:00

--- supplyTrack/src/routes/devolucaoRoutes.js
+++ supplyTrack/src/routes/devolucaoRoutes.js
@@ -6,7 +6,14 @@ const devolucaoController = require('../controllers/devolucaoController');
 // Rota para cliente buscar suas próprias devoluções
 router.get('/cliente/devolucoes/:idUsuario', devolucaoController.getDevolucoesByClienteId);
 
-// Rota para gestor buscar todas as devoluções
+// Rota para gestor buscar todas as devoluções (Painel Gestor)
 router.get('/gestor/devolucoes', devolucaoController.getAllDevolucoes);
+
+// **NOVA ROTA:** Para o cliente criar uma nova devolução
+router.post('/cliente/nova-devolucao', devolucaoController.createNewDevolucao);
+
+// **NOVA ROTA:** Para o histórico geral de devoluções (pode ser acessada pelo gestor)
+router.get('/historico/devolucoes', devolucaoController.getHistoricoDevolucoes);
+
 
 module.exports = router;

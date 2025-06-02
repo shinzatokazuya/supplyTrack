1e592ceea7958b87251591900020fd172a54b5da
Kazuya <shinzatokazuya11@gmail.com>
2025-06-02 20:26:28 +0000

    Mon, Jun 2, 2025, 5:26 PM -03:00

--- supplyTrack/app.js
+++ supplyTrack/app.js
@@ -3,18 +3,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const authRoutes = require('./src/routes/authRoutes');
+const devolucaoRoutes = require('./src/routes/devolucaoRoutes'); 
 
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
 // Rotas da API
 app.use('/api/auth', authRoutes);
-// app.use('/api/clientes', clienteRoutes);
-// app.use('/api/gestores', gestorRoutes);
+app.use('/api', devolucaoRoutes); // **NOVO: Usar rotas de devolução com prefixo /api**
 
 // Rota principal para a página inicial
 app.get('/', (req, res) => {

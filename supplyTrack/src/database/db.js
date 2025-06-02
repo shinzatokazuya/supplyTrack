cce872bbf2de945eaa8fbd7e890abaf9b1a22540
Kazuya <shinzatokazuya11@gmail.com>
2025-06-02 21:51:11 +0000

    Mon, Jun 2, 2025, 6:51 PM -03:00

--- supplyTrack/src/database/db.js
+++ supplyTrack/src/database/db.js
@@ -2,7 +2,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, '..', 'SupplyTrack.db'); // Garante o caminho correto para o DB
+const dbPath = path.resolve(__dirname, '..', '..', 'SupplyTrack.db'); // Garante o caminho correto para o DB
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados:', err.message);

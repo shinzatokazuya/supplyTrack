e527918a448406626a52501a8d6112e907a2f8bb
Kazuya <shinzatokazuya11@gmail.com>
2025-09-07 20:39:12 +0000

    Sun, Sep 7, 2025, 5:39 PM -03:00

--- supplyTrack/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/cadastro', authController.cadastrarUsuario);
-router.post('/login', authController.login)
-
-module.exports = router;
--- supplyTrack/src/routes/produtoRoutes.js
+++
@@ -1,8 +0,0 @@
-// src/routes/produtoRoutes.js
-const express = require('express');
-const router = express.Router();
-const produtoController = require('../controllers/produtoController');
-
-router.get('/produtos', produtoController.getAllProdutos);
-
-module.exports = router;

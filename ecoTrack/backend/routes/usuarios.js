06f2562d444cb3f7ea44d83d28362d0fa64c4774
Kazuya <shinzatokazuya11@gmail.com>
2025-11-15 23:51:52 +0000

    Sat, Nov 15, 2025, 8:51 PM -03:00

---
+++ ecoTrack/backend/routes/usuarios.js
@@ -0,0 +1,56 @@
+import { Router } from "express";
+import { openDb } from "../bd/connection.js";
+
+const router = Router();
+
+// LISTAR
+router.get("/", async (req, res) => {
+  const db = await openDb();
+  const rows = await db.all("SELECT * FROM usuarios");
+  res.json(rows);
+});
+
+// BUSCAR POR ID
+router.get("/:id", async (req, res) => {
+  const db = await openDb();
+  const row = await db.get("SELECT * FROM usuarios WHERE ID=?", [req.params.id]);
+  res.json(row);
+});
+
+// CRIAR
+router.post("/", async (req, res) => {
+  const db = await openDb();
+  const { nome, email, ra, curso_id, campus_id, tipo_usuario, senha } = req.body;
+
+  const sql = `
+    INSERT INTO usuarios (nome, email, ra, curso_id, campus_id, tipo_usuario, senha)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  const r = await db.run(sql, [nome, email, ra, curso_id, campus_id, tipo_usuario, senha]);
+  res.json({ ID: r.lastID });
+});
+
+// ATUALIZAR
+router.put("/:id", async (req, res) => {
+  const db = await openDb();
+  const { nome, email, ra, curso_id, campus_id, tipo_usuario, senha } = req.body;
+
+  const sql = `
+    UPDATE usuarios
+    SET nome=?, email=?, ra=?, curso_id=?, campus_id=?, tipo_usuario=?, senha=?
+    WHERE ID=?
+  `;
+
+  await db.run(sql, [nome, email, ra, curso_id, campus_id, tipo_usuario, senha, req.params.id]);
+  res.json({ message: "Atualizado" });
+});
+
+// DELETAR
+router.delete("/:id", async (req, res) => {
+  const db = await openDb();
+  await db.run("DELETE FROM usuarios WHERE ID=?", [req.params.id]);
+  res.json({ message: "Removido" });
+});
+
+export default router;

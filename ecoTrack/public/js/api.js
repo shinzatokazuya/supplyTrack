a041e823048d943b07c971fe8bfe79e688d097b3
Kazuya <shinzatokazuya11@gmail.com>
2025-11-15 21:20:31 +0000

    Sat, Nov 15, 2025, 6:20 PM -03:00

--- ecoTrack/public/js/api.js
+++ ecoTrack/public/js/api.js
@@ -8,8 +8,33 @@ async function apiGet(path) {
 }
 
 async function apiPost(path, body) {
-    const res = await fetch(`${API_BASE}${path}`), {
+    const res = await fetch(`${API_BASE}${path}`, {
         method: "POST",
-        
-    }
+        headers: {" Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    });
+    if (!res.ok) throw new Error(await res.text());
+    return res.json();
+}
+
+async function apiPut(path, body) {
+    const res = await fetch(`${API_BASE}${path}`, {
+        method: "PUT",
+        headers: {" Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    });
+    if (!res.ok) throw new Error(await res.text());
+    return res.json();
+}
+
+async function apiDelete(path) {
+    const res = await fetch(`${API_BASE}${path}`, {
+        method: "DELETE"
+    });
+    if (!res.ok) throw new Error(await res.text());
+    return res.json();
+}
+
+function showError(err) {
+    alert("ERRO: " + (err.message||err));
 }

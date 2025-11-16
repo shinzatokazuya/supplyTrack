8ee3351368d0bf70480d554d81808b7ea19109b3
Kazuya <shinzatokazuya11@gmail.com>
2025-11-16 01:06:09 +0000

    Sat, Nov 15, 2025, 10:06 PM -03:00

--- ecoTrack/backend/bd/connection.js
+++ ecoTrack/backend/bd/connection.js
@@ -1,9 +1,9 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
 
 export async function openDb() {
     return open({
-        filename: "./bd/ecoTrack.db",
+        filename: './bd/ecoTrack.db',
         driver: sqlite3.Database
     });
 }
